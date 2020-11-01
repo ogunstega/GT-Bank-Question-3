65a405d1522794bce0c83e43aa17e2baa72143fd
ogunstega <ogunstega@gmail.com>
2020-11-02 00:29:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GT_Bank_Question_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine(test("abcd"));
+                
+                Console.ReadLine();
+            }
+
+
+        }
+        public static string test(string str)
+        {
+            return str.Length > 1
+                ? str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1) : str;
+        }
+    }    
+}
